27ff240df9dad0c72bbd61c281b913be6b173a11
nagasawa <nagasawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-12-26 05:09:00 +0000

    changed the layout of Delete and Delete and fixed weakness
    git-svn-id: https://svn.openpne.jp/svn/OpenPNE3/plugins/opIntroEssayPlugin/trunk@9690 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ apps/mobile_frontend/modules/introessay/templates/deleteSuccess.php
@@ -0,0 +1,4 @@
+<?php include_page_title('本当に削除しますか？') ?>
+<form style="width: 48%; float: left; text-align: right" action="" method="post"><input type="hidden" name="delete" value="1" /><input type="submit" value="はい" /></form>
+<form style="width: 48%; float: right; text-align: left" action="" method="post"><input type="submit" value="いいえ" /></form>
+<div style="clear: left"></div>
