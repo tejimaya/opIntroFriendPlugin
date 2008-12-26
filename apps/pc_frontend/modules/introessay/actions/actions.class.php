1ff97c2b14a6e27745830a7dad1989ecadccd2a6
nagasawa <nagasawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-12-26 04:56:54 +0000

    changed the layout of Delete and
    git-svn-id: https://svn.openpne.jp/svn/OpenPNE3/plugins/opIntroEssayPlugin/trunk@9686 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/introessay/actions/actions.class.php
+++ apps/pc_frontend/modules/introessay/actions/actions.class.php
@@ -8,59 +8,14 @@
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
  */
-class introessayActions extends sfActions
+class introessayActions extends opIntroEssayPluginIntroEssayActions 
 {
  /**
-  * Executes index action
+  * Executes show action
   *
   * @param sfRequest $request A request object
   */
-  public function executeIndex($request)
-  {
-    $this->id = $request->getParameter('id');
-    $this->introEssay = IntroEssayPeer::getByFromAndTo($this->getUser()->GetMemberId(), $this->id);
-    $this->form = new IntroEssayForm($this->introEssay);
-    if ($request->isMethod('post'))
-    {
-      $array = $request->getParameter('intro_essay');
-      if (!$this->introEssay)
-      {
-        $array['from_id'] = $this->getUser()->GetMemberId();
-        $array['to_id'] = $this->id;
-      }
-      $this->form->bind($array);
-      if ($this->form->isValid())
-      {
-        if ($this->introEssay)
-        {
-          $this->introEssay->setContent($array['content']);
-          $this->introEssay->save();
-        }
-        else
-        {
-          $this->form->save();
-        }
-        return sfView::SUCCESS;
-      }
-    }
-    return sfView::INPUT;
-  }
-
-  public function executeDeleteInfo($request)
-  {
-    $this->id = $request->getParameter('id');
-  }
-
-  public function executeDelete($request)
-  {
-    $this->id = $request->getParameter('id'); 
-    $this->introEssay = IntroEssayPeer::getByFromAndTo($this->getUser()->GetMemberId(), $this->id);
-    $this->delete();
-    redirect("member/?id=" . $this->id);
-  }
-
   public function executeShow($request)
   {
-    $this->id = $request->getParameter('id');
   }
 }
--- apps/pc_frontend/modules/introessay/templates/deleteSuccess.php
+++ apps/pc_frontend/modules/introessay/templates/deleteSuccess.php
@@ -1,1 +1,6 @@
-<?php include_box( 'infobox', '紹介文を作成する', '削除しました') ?>
+<?php
+$html = '<form style="width: 48%; float: left; text-align: right" action="" method="post"><input type="hidden" name="delete" value="1" /><input type="submit" value="はい" /></form>'
+      . '<form style="width: 48%; float: right; text-align: left" action="" method="post"><input type="submit" value="いいえ" /></form>'
+      . '<div style="clear: left"></div>';
+include_box( 'infobox', '本当に削除しますか？', $html);
+
