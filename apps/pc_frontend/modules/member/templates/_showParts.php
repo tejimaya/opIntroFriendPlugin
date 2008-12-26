07252ade3a444b3873cda6b78217340a0513a44e
nagasawa <nagasawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-12-26 07:51:50 +0000

    added The number of lines limitation
    git-svn-id: https://svn.openpne.jp/svn/OpenPNE3/plugins/opIntroEssayPlugin/trunk@9704 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/member/templates/_showParts.php
+++ apps/pc_frontend/modules/member/templates/_showParts.php
@@ -13,7 +13,16 @@ if (count($introEssays)) {
       $criteria->add(MemberPeer::ID, $introEssay->getFromId());
       $writer = MemberPeer::doSelectOne($criteria);
 
-      $list[link_to(image_tag_sf_image($writer->getImage(), array('size' => '76x76')) . '<br />' . $writer->getName(), 'member/' . $writer->getId())] = nl2br($introEssay->getContent());
+      $string = $introEssay->getContent();
+      $pos = 0;
+      for ($i = 0; $i < sfConfig::get('app_line_introessay'); $i++)
+      {
+        $pos = mb_strpos($string, "\n", $pos + 1);
+        if (!$pos) { break; }
+      }
+      if ($pos) { $string = mb_substr($string, 0, $pos - 1); }
+      $name = link_to(image_tag_sf_image($writer->getImage(), array('size' => '76x76')) . '<br />' . $writer->getName(), 'member/' . $writer->getId());
+      $list[$name] = nl2br($string) . ($pos ? '...' : '');
   }
   $list['　'] = link_to( '全て見る', 'introessay/show?id=' . $id );
   include_list_box('introEssay', $list, array( 'title' => 'マイフレンドからの紹介文'));
