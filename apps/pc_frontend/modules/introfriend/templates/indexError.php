0ca216ac3100a66dcd7e3247de89bf4db4ca2408
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-24 18:26:51 +0000

    #3360:added "back to previous page" to the every *Error templates of the pc_* application
    git-svn-id: https://svn.openpne.jp/svn/OpenPNE3/plugins/opIntroFriendPlugin/trunk@10361 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/introfriend/templates/indexError.php
+++ apps/pc_frontend/modules/introfriend/templates/indexError.php
@@ -1,2 +1,4 @@
-<?php
-include_box( 'notFriend', __('Error'), __('Not your friend'));
+<?php include_box( 'notFriend', __('Error'), __('Not your friend')); ?>
+
+<?php use_helper('Javascript') ?>
+<p><?php echo link_to_function(__('前のページに戻る'), 'history.back()') ?></p>
