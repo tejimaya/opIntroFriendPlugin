b0d0fd8dde28f48ba926a20376c482245ef79ff1
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-27 03:43:31 +0000

    opIntroFriendPlugin: op_include_line
    git-svn-id: https://svn.openpne.jp/svn/OpenPNE3/plugins/opIntroFriendPlugin/trunk@10476 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/introfriend/templates/indexError.php
+++ apps/pc_frontend/modules/introfriend/templates/indexError.php
@@ -1,4 +1,4 @@
-<?php include_box( 'notFriend', __('Error'), __('Not your friend')); ?>
+<?php include_box('notFriend', __('Error'), __('Not your friend')); ?>
 
 <?php use_helper('Javascript') ?>
-<p><?php echo link_to_function(__('前のページに戻る'), 'history.back()') ?></p>
+<?php op_include_line('backLink', link_to_function(__('Back to previous page'), 'history.back()')) ?>
