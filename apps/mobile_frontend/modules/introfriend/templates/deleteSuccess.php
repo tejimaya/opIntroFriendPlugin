3afe71025b5cee89a6e1985669f00890e74c9d48
Masato Nagasawa <nagasawa@tejimaya.com>
2010-08-10 20:05:16 +0900

    fixed added CSRF token at delete of the introduction friend action in the mobile.

--- apps/mobile_frontend/modules/introfriend/templates/deleteSuccess.php
+++ apps/mobile_frontend/modules/introfriend/templates/deleteSuccess.php
@@ -1,4 +1,11 @@
 <?php op_mobile_page_title(__('Do you really delete it?')) ?>
-<form style="width: 48%; float: left; text-align: right" action="" method="post"><input type="hidden" name="delete" value="1" /><input type="submit" value="<?php echo __('Yes') ?>" /></form>
-<form style="width: 48%; float: right; text-align: left" action="" method="post"><input type="submit" value="<?php echo __('No')?>"  /></form>
+<?php $form = new sfForm() ?>
+<form style="width: 48%; float: left; text-align: right" action="" method="post">
+<input type="hidden" name="delete" value="1">
+<input type="hidden" name="<?php echo $form->getCSRFFieldName() ?>" value="<?php echo $form->getCSRFToken() ?>">
+<input type="submit" value="<?php echo __('Yes') ?>">
+</form>
+<form style="width: 48%; float: right; text-align: left" action="" method="post">
+<input type="submit" value="<?php echo __('No')?>">
+</form>
 <div style="clear: left"></div>
