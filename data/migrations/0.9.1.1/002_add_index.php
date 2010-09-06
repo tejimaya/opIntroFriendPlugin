a20b6a047b0d326e6c3cd59a5c01ad0fd33eba5e
Masato Nagasawa <nagasawa@tejimaya.com>
2010-09-06 21:36:23 +0900

    relation で作成されるインデックスを追加していたのを修正 (fixes #1549)

--- data/migrations/0.9.1.1/002_add_index.php
+++ data/migrations/0.9.1.1/002_add_index.php
@@ -12,8 +12,6 @@ class Revision2_addIndex extends opMigration
   public function migrate($direction)
   {
     $this->addIndex('intro_friend', 'created_at_idx', array('fields' => array('created_at')));
-    $this->addIndex('intro_friend', 'member_id_to_idx', array('fields' => array('member_id_to')));
-    $this->addIndex('intro_friend', 'member_id_from_idx', array('fields' => array('member_id_from')));
     $this->addIndex('intro_friend', 'member_id_from_member_id_to_idx', array('fields' => array('member_id_from', 'member_id_to')));
   }
 }
