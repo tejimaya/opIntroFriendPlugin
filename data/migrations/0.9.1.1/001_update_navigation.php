13542bc24423fe67e40720ab758bf5d86ebb31b0
Masato Nagasawa <nagasawa@tejimaya.com>
2010-09-06 13:53:48 +0900

    tuning migration. (fixes #1550)

--- data/migrations/0.9.1.1/001_update_navigation.php
+++ data/migrations/0.9.1.1/001_update_navigation.php
@@ -11,11 +11,10 @@ class Revision1_updateNavigation extends opMigration
 {
   public function migrate($direction)
   {
-    $naviList = Doctrine::getTable('Navigation')->findByUri('introfriend/index');
-    foreach ($naviList as $navi)
-    {
-      $navi->setUri('@introfriend');
-      $navi->save();
-    }
+    opDoctrineQuery::create()
+      ->update('Navigation n')
+      ->set('n.uri', '@introfriend')
+      ->where('n.uri = ?', 'introfriend/index')
+      ->execute();
   }
 }
