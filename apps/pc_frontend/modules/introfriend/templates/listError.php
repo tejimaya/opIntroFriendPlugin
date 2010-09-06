aef03bb4e3c48bd808aad74902fae1f3a68b5d7a
Masato Nagasawa <nagasawa@tejimaya.com>
2010-09-06 18:51:06 +0900

    フレンド一覧ページで紹介文がない場合のページを追加 (fixes #1552)

---
+++ apps/pc_frontend/modules/introfriend/templates/listError.php
@@ -0,0 +1,4 @@
+<?php include_box('notFriend', __('Error'), __('A friend\'s introductory essay does not exists.')); ?>
+
+<?php use_helper('Javascript') ?>
+<?php op_include_line('backLink', link_to_function(__('Back to previous page'), 'history.back()')) ?>
