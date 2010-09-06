e924770a99cd6d2c3ffb39ad4d8ec8b6641c2ca6
Masato Nagasawa <nagasawa@tejimaya.com>
2010-09-06 19:33:31 +0900

    プロフィールページのリンク修正 (fixes #1552)

--- apps/mobile_frontend/modules/introfriend/templates/_introFriendLink.php
+++ apps/mobile_frontend/modules/introfriend/templates/_introFriendLink.php
@@ -2,11 +2,11 @@
 
 $cnt = Doctrine::getTable('IntroFriend')->getCount($id);
 
-echo link_to(__('Read introductory essay') . '(' . $cnt . ')', 'obj_introfriend' . $member);
+echo link_to(__('Read introductory essay') . '(' . $cnt . ')', 'obj_introfriend', $member);
 
 if ($isFriend == true)
 {
-  echo ' / ' . link_to(__('Write'), 'obj_member_introfriend=', $member);
+  echo ' / ' . link_to(__('Write'), 'obj_member_introfriend', $member);
 }
 
 ?>
