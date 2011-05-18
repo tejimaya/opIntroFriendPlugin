0f0ae26934531ea4d3e9b48d2d6ebdeee0596821
nise_nabe <nise.nabe@gmail.com>
2011-05-18 15:16:19 +0900

    fixed to print which form are required (refs #2110)

--- apps/mobile_frontend/modules/introfriend/templates/indexInput.php
+++ apps/mobile_frontend/modules/introfriend/templates/indexInput.php
@@ -1,7 +1,5 @@
 <?php op_mobile_page_title(__('Create introductory essay')) ?>
-<form action="<?php echo url_for('obj_member_introfriend', $member) ?>" method="post">
-<?php echo $form ?>
-<br><br>
-<center><input type="submit" value="<?php echo __('Create') ?>"></center>
-</form>
-
+<?php op_include_form('InfroFriend', array($form), array(
+  'button' => __('Create'),
+  'align' => 'center',
+))?>
