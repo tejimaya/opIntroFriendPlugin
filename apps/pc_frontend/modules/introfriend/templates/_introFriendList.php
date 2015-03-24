77f2e05ac56613d33456342b100ed0f3fc1990af
nishizoe <nishizoe@tejimaya.com>
2015-03-24 15:03:02 +0900

    (refs #2129) If the viewer is not logged in, you do not want to display the information in the profile private members

--- apps/pc_frontend/modules/introfriend/templates/_introFriendList.php
+++ apps/pc_frontend/modules/introfriend/templates/_introFriendList.php
@@ -3,13 +3,24 @@
 if (count($introList))
 {
   $list = array();
+  $i = 0;
   foreach ($introList as $obj)
   {
     $member = $obj['member'];
     $pos = 0;
-    $img = link_to(image_tag_sf_image($member->getImageFilename(), array('size' => '76x76'))
-         .'<br />'.$member->getName(), 'member/'.$member->getId());
-    $list[$img] = nl2br($obj['essay']);
+    $num = $i++;
+    if('4' === $member->getConfig('profile_page_public_flag') || $sf_user->isSNSMember())
+    {
+      $img = link_to(image_tag_sf_image($member->getImageFilename(), array('size' => '76x76'))
+      .'<br />'.$member->getName(), 'member/'.$member->getId());
+      $list[$img] = nl2br($obj['essay']);
+    }
+    else
+    {
+      $img = op_image_tag('no_image.gif', array('size' => '76x76'))
+      .'<br /><!--'. $num .'-->'.__('Private<br />Member');
+      $list[$img] = __('Private Introductory essay');
+    }
   }
   $list[' '] = link_to(__('Show all').'('.$count.')', '@obj_introfriend?id='.$id);
   include_list_box('introFriend', $list, array('title' => __('Introductory essay from %my_friend%')));
